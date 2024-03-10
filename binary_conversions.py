2e91e93ac080ce0abaa2cadedaee74142f41a7c9
YarinAtias <yarinatias212@gmail.com>
2024-03-10 21:01:00 +0200

    Fixing negative binary conversation

--- binary_conversions.py
+++ binary_conversions.py
@@ -32,11 +32,23 @@ def negative_decimal_to_binary(number: int):
     added_1 = False
     index = 0
     while (not added_1) and (index <= 7):
+        remember_1 = False
         current_bit = binary_in_negative[7 - index]
-        if current_bit == '0':
+        if current_bit == '1':
+            binary_in_negative[7 - index] = '0'
+            remember_1 = True
+
+        elif current_bit == '0' and not remember_1:
             binary_in_negative[7 - index] = '1'
             added_1 = True
+
+        if current_bit == '0' and remember_1:
+            binary_in_negative[7 - index] = '1'
+            added_1 = False
+
         index = index + 1
     binary_in_negative = "".join(binary_in_negative)
     return binary_in_negative
 
+
+print(negative_decimal_to_binary(-100))
