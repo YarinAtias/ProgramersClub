af3aad6e0a509fb2b3f4e2723584ab0e3d7180f6
YarinAtias <yarinatias212@gmail.com>
2024-03-20 17:28:41 +0200

    Adding a way to pass a file with sys module

--- most_common_words.py
+++ most_common_words.py
@@ -1,15 +1,22 @@
 import re
+import sys
 
 def remove_non_letters(word):
     return re.sub(r'[^a-zA-Z]', '', word)
 
 def print_most_common_words(file: str, N=None) -> None:
-    with open(file, 'r') as f:
-        file_to_read = f.read()
+    try:
+        with open(file, 'r') as f:
+            file_to_read = f.read()
+    except:
+        file_to_read = file
 
     list_of_word = [remove_non_letters(word.strip()) for word in file_to_read.split(" ") if word != "" and len(word) > 1]
     words_counter_dic = {word: list_of_word.count(word) for word in list_of_word}
-    words_counter_dic.pop("")
+    try:
+        words_counter_dic.pop("")
+    except:
+        pass
     words_counter_dic = sorted(words_counter_dic.items(), key=lambda item: item[1], reverse=True)
 
     for i, item in enumerate(words_counter_dic):
@@ -20,3 +27,11 @@ def print_most_common_words(file: str, N=None) -> None:
         except IndexError:
             break
 
+
+def main():
+    file_content = sys.argv[1]
+    print_most_common_words(file_content)
+
+
+if __name__ == "__main__":
+    main()
