518448c764cdfedff3fc3e293dd90da83619ccb2
YarinAtias <yarinatias212@gmail.com>
2024-06-11 17:36:09 +0300

    Adding a function which prints all files and subfolders in a given path (Recursive)

---
+++ client_server/files_client.py
@@ -0,0 +1,92 @@
+import os.path
+import socket
+import shutil
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+END_SESSION_MESSAGE = "-999"
+FAILED_FINDING_MESSAGE = "This file is not available, try again please."
+
+IP = "127.0.0.1"
+PORT = 8820
+
+client_socket.connect((IP, PORT))
+run_client = True
+FIRST_TIME_ASKED = False
+user_choice = '0'
+
+def download_files():
+    global run_client
+    global FIRST_TIME_ASKED
+
+    received_data = client_socket.recv(1024).decode()
+    if not FIRST_TIME_ASKED:
+        print(received_data)
+        data_to_send = input("Enter the file you want to get: ").encode()
+        client_socket.send(data_to_send)
+        FIRST_TIME_ASKED = True
+    else:
+        if received_data == END_SESSION_MESSAGE:
+            print("Goodbye.")
+            run_client = False
+        elif received_data == FAILED_FINDING_MESSAGE:
+            print(received_data)
+            data_to_send = input("Enter the file you want to get: ").encode()
+            client_socket.send(data_to_send)
+        else:
+            print(received_data)
+            run_client = False
+
+
+def upload_files():
+    global run_client
+    file_to_upload = input("Enter file path (Enter -999 to end the session): ")
+    if file_to_upload == END_SESSION_MESSAGE:
+        print("Goodbye.")
+        client_socket.send(END_SESSION_MESSAGE.encode())
+        run_client = False
+    else:
+        if os.path.exists(file_to_upload) and os.path.isfile(file_to_upload):
+            file_name = os.path.basename(file_to_upload)
+            if file_name in os.listdir("saved_files"):
+                print("This file is already in the directory. Try again please.")
+            else:
+                shutil.copy(file_to_upload, "saved_files")
+                print("File has uploaded!")
+                client_socket.send(END_SESSION_MESSAGE.encode())
+                run_client = False
+        else:
+            print("Did not work, try again please.")
+
+
+def user_choice_func():
+    user_choice_text = client_socket.recv(1024).decode()
+    print(user_choice_text)
+    global user_choice
+    user_choice = input()
+
+def main():
+    user_choice_func()
+    global user_choice
+    ALREADY_CHOOSE = False
+
+    while run_client:
+        if user_choice == '1':
+            if not ALREADY_CHOOSE:
+                client_socket.send('1'.encode())
+                ALREADY_CHOOSE = True
+            else:
+                download_files()
+        elif user_choice == '2':
+            if not ALREADY_CHOOSE:
+                client_socket.send('2'.encode())
+                ALREADY_CHOOSE = True
+            else:
+                upload_files()
+        else:
+            print("Choice wasn't recognized. Please try again")
+            user_choice = input()
+
+
+if __name__ == '__main__':
+    main()
+client_socket.close()
---
+++ client_server/files_server.py
@@ -0,0 +1,75 @@
+import socket
+import os
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+IP = "127.0.0.1"
+PORT = 8820
+FAILED_FINDING_MESSAGE = "This file is not available, try again please."
+END_SESSION_MESSAGE = "-999"
+
+server_socket.bind((IP, PORT))
+run_server = True
+run_client = True
+ALREADY_SENT_DIR = False
+
+print("Server is up and running")
+
+def download_files():
+    global run_client
+    global run_server
+    global ALREADY_SENT_DIR
+
+    if not ALREADY_SENT_DIR:
+        data_to_send = f"""Files to download\n{chr(10).join(files_directory)}\n(Enter -999 to finish the session)
+            """
+        ALREADY_SENT_DIR = True
+        client_socket.send(data_to_send.encode())
+    else:
+        data_received = client_socket.recv(1024).decode()
+        if data_received == END_SESSION_MESSAGE:
+            run_client = False
+            run_server = False
+            client_socket.send(END_SESSION_MESSAGE.encode())
+            client_socket.close()
+        else:
+            if data_received in files_directory:
+                print("Check")
+                run_client = False
+                run_server = False
+                client_socket.send(f"Downloading {data_received}...".encode())
+            else:
+                client_socket.send(FAILED_FINDING_MESSAGE.encode())
+
+
+def upload_files():
+    global run_client
+    global run_server
+    try:
+        message = client_socket.recv(1024).decode()
+        if message == END_SESSION_MESSAGE:
+            run_client = False
+            run_server = False
+    except:
+        pass
+
+def user_choice_func():
+    user_choice_text = "Choose 1 to download files\nChoose 2 to upload a file".encode()
+    client_socket.send(user_choice_text)
+
+
+while run_server:
+    server_socket.listen()
+    (client_socket, client_address) = server_socket.accept()
+    print("Client connected")
+    files_directory = os.listdir("saved_files")
+    user_choice_func()
+    user_choice = client_socket.recv(1024).decode()
+    while run_client:
+        if user_choice == '1':
+            download_files()
+        elif user_choice == '2':
+            upload_files()
+
+
+
+server_socket.close()
